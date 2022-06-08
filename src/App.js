fc66c1136d3ee82c22f6333c323a73d4a9c1f55a
Yue Ben <benyue0521@gmail.com>
2022-06-08 17:03:50 +0200

    Import React.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,15 +9,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my world!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.dr.dk/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Read News
         </a>
       </header>
     </div>
